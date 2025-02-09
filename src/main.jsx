0e52f0918fe650c06ae60c482c95a367576bc21f
s2322087 <s2322087@stu.musashino-u.ac.jp>
2025-02-10 02:09:17 +0900

    apiからスレッド一覧の取得

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
