180449eda9a471a2da93b87c8b6b7e340f0832f1
s2322087 <s2322087@stu.musashino-u.ac.jp>
2025-02-14 21:25:14 +0900

    スレッド内のタイトル表示とoffsetの切り替えの実装

--- src/ThreadDetail.jsx
+++ src/ThreadDetail.jsx
@@ -1,20 +1,27 @@
 import React, {useEffect, useState} from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
+import './App.css'
 
 function ThreadDetail() {
-    const [thread, setThread] = useState();
+    const location = useLocation();
+    const [thread, setThread] = useState(null);
     const {id} = useParams();
+    const [offset, setOffset] = useState(0);
     const [error, setError] = useState("");
+    const {title} =location.state || {};
 
-    async function fetchThread(threadId){
+
+    async function fetchThread(threadId, Offset){
         try{
         const api = import.meta.env.VITE_API_URL;
-        const response = await fetch(`${api}/threads/${threadId}/posts`);
+        const response = await fetch(`${api}/threads/${threadId}/posts?offset=${Offset}`);
         if (!response.ok) {
             throw new Error(`Error! status: ${response.status}`);
           }
         const data = await response.json();
         setThread(data);
+
+    
         } catch (err) {
             console.error(err);
             setError("スレッドを読み込めませんでした。")
@@ -23,12 +30,37 @@ function ThreadDetail() {
 
     useEffect(() => {
         if (id){
-            fetchThread(id);
+            fetchThread(id, offset);
         }
-    }, [id]);
-    return(
+    }, [id, offset]);
+
+    const handleNext = () => {
+        setOffset((prevOffset) => prevOffset + 10);
+      };
+    
+    const handlePrev = () => {
+        setOffset((prevOffset) => Math.max(0, prevOffset - 10))
+      }
+
+
+    if (!thread) {
+        return <p>スレッドを読み込み中。</p>;
+      }
+    
+    return (
         <div>
-          <h2>a</h2>
+          <h2>{title}</h2>
+            <ul>
+              {thread.posts.map((p) => (
+              <li key={p.id}>
+                {p.post}
+              </li>
+                ))}
+            </ul>
+          <div className="button-group">
+            <button onClick={handlePrev} disabled={offset === 0} className="paging-button">＜</button>
+            <button onClick={handleNext} disabled={thread.posts.length < 10}  className="paging-button">＞</button>
+          </div>
         </div>
       );
 }
